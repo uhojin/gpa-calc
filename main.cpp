a9666e138d92b572add7ab7013b25ddc68ec4363
Hojin You <youho@sheridancollege.ca>
2023-01-17 22:11:04 -0500

    Copied boilerplates from assignment document, and added Course class.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#define MAX_CHAR 99
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+class Course {
+private:
+    int courseID;
+    int courseCredit;
+    float grade;
+
+public:
+    Course(int id, int credit, float g) {
+        courseID = id;
+        courseCredit = credit;
+        grade = g;
+    }
+
+    int getCredits() {
+        return courseCredit;
+    }
+
+    float getGrade() {
+        return grade;
+    }
+
+};
+
+
+int main(int argc, char** argv) {
+    char str[MAX_CHAR];
+    cout << "Enter Student Name: ";
+    fgets(str, MAX_CHAR, stdin);
+    RMN(str);
+    cout << str << endl;
+    Student *s = new Student(str);
+    s -> menu();
+    delete s;
     return 0;
 }
